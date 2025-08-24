cd60fb2be2bb05ba294146d9a35dc12e4dde571b
Shubham Kumar <shubhamkumar200328@gmail.com>
2025-08-24 18:14:02 +0530

    Fetching Data with server components

---
+++ src/app/users-server/page.tsx
@@ -0,0 +1,51 @@
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  address: {
+    city: string;
+  };
+  company: {
+    name: string;
+  };
+};
+
+export default async function usersServer() {
+  let users: User[] = [];
+
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch users: ${response.statusText}`);
+    }
+
+    users = await response.json();
+  } catch (error) {
+    console.error("Fetch error:", error);
+  }
+
+  return (
+    <div className="m-15 p-3 bg-black text-center">
+      <ul className="flex flex-wrap gap-4 m-auto">
+        {users.map((user) => (
+          <li
+            key={user.id}
+            className="w-64 bg-white shadow-md rounded-lg p-4 text-gray-700"
+          >
+            <div className="font-bold text-gray-900">{user.name}</div>
+            <div className="text-sm mt-2 space-y-1">
+              <div>Username: {user.username}</div>
+              <div>Email: {user.email}</div>
+              <div>Phone: {user.phone}</div>
+              <div>City: {user.address.city}</div>
+              <div>Company: {user.company.name}</div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
